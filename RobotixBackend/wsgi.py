c6a43885c61dd83095658c0780b90d654896892f
Kumar Utsav <utsavkumar.9749@gmail.com>
2023-05-05 14:25:49 +0530

    Added Staticfiles

--- RobotixBackend/wsgi.py
+++ RobotixBackend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoauthapi1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RobotixBackend.settings')
 
 application = get_wsgi_application()
